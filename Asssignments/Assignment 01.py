b9b7df5bb50f3d9695e3d1ac77b6267aed36e962
valvmng <60613315+valvmng@users.noreply.github.com>
2020-07-26 14:56:36 -0400

    Initial commit

---
+++ Asssignments/Assignment 01.py
@@ -0,0 +1,10 @@
+# Problem: If you had deposited a coin on the cryptocurrency exchange that brought 7% fixed profit daily for a week, how much would your $ 1000 reach at the end of the 7th day?
+
+rate = .07
+present_value = 1000
+periods = 7
+
+future_value = present_value * (1 + rate) ** periods  # future value
+future_value_str = str(round(future_value, 2))
+answer=f"At the end of the {periods}th day my original investment of ${present_value} at {rate} rate will have a value of ${future_value_str}."
+print(answer) 
